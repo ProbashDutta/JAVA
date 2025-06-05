3cdf1b59520f85a4d277f605c9beaa977edb2552
ProbashDutta <PROBASHDUTTA10@GMAIL.COM>
2025-06-06 02:20:02 +0530

    13

--- variables.java
+++ variables.java
@@ -1,10 +1,13 @@
 public class  variables {
     public static void main(String[] args) {
 
-        int a = -5;
-        int b = 8;
-        int c = 6;
-        int result = (a+b*c);
+        int a = 5;
+        int b = 15;
+        int c = 3;
+        int d = 2;
+        int e =8;
+        
+        int result = (a+b/c*d-e%c);
     
         System.out.println(result);
 
