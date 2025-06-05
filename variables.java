c5ded46b24e8d48971790c6bb3443549fbef1375
ProbashDutta <PROBASHDUTTA10@GMAIL.COM>
2025-06-05 23:04:51 +0530

    variables

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class  variables {
+    public static void main(String[] args) {
+
+        int a = 25;
+        int b = 10;
+        int sum = a+b;
+        System.out.println(sum);
+
+    }
+    
+}
