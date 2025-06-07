fcd2597e9eeb81744baccda8e52c5875242d4643
ProbashDutta <PROBASHDUTTA10@GMAIL.COM>
2025-06-07 21:37:10 +0530

    7

--- condition.java
+++ condition.java
@@ -1,14 +1,21 @@
 import java.util.*;
-
 public class condition {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
 
-        if(age > 18) {
-            System.out.println("You are an adult");
-        } else {
-            System.out.println("Not an adult");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+        if(a==b) {
+            System.out.println("Equal");
         }
+        else if(a>b) {
+            System.out.println("a is greater");
+        }
+        else {
+            System.out.println("a is lesser");
+        }
+        }
+
     }
-}
