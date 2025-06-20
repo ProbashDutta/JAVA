b09e72921dc435749c67dde5a0c190ac043bdf67
ProbashDutta <PROBASHDUTTA10@GMAIL.COM>
2025-06-20 21:16:27 +0530

    ptrn 33434

--- loops.java
+++ loops.java
@@ -2,9 +2,6 @@ public class loops {
 
     public static void main(String args[]) {
 
-       for(int counter = 0; counter < 11; counter++) {
-          System.out.println(counter);
-       }
+      
 
-    }
-}
+       
---
+++ pattern/patterns1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class patterns1 {
+     
+    public static void main(String args[]) {
+        int n = 4;
+        int m = 5;
+
+        for(int i=1; i<=n; i++) {
+             
+                for(int j=i; j<=m; j++) {
+                    System.out.println("*");
+                System.out.println();
+            }
+        }
+}
+}
